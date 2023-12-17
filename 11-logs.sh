9c9e524097c8487b10daffe09128d4c7ae27ab4f
daws76 <schandradba9@gmail.com>
2023-12-17 20:36:24 +0530

    script updated

--- 11-logs.sh
+++ 11-logs.sh
@@ -3,6 +3,9 @@
 ID=$(id -u)
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 echo "script name: $0"
 
@@ -11,15 +14,15 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
      if [ $1 -ne 0 ]
      then
-       echo "Error $2 ... Failed"
+       echo -e "Error $2 ... $R Failed $N"
     else 
-       echo "$2 ... Success"
+       echo "$2 ... $G Success $N"
     fi   
 }
 
 if [ $ID -ne 0 ]
 then 
-  echo "Error please run with root user"
+  echo -e "$R Error please run with root user $N"
 else  
   echo "You are root user"
 fi 

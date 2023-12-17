38c3bc0ecce56801e2edde8dd95f2b8a60468fb6
daws76 <schandradba9@gmail.com>
2023-12-17 20:38:48 +0530

    script updated

--- 11-logs.sh
+++ 11-logs.sh
@@ -15,6 +15,7 @@ VALIDATE(){
      if [ $1 -ne 0 ]
      then
        echo -e "Error $2 ... $R Failed $N"
+       exit 1
     else 
        echo "$2 ... $G Success $N"
     fi   
@@ -23,6 +24,7 @@ VALIDATE(){
 if [ $ID -ne 0 ]
 then 
   echo -e "$R Error please run with root user $N"
+  exit 1
 else  
   echo "You are root user"
 fi 

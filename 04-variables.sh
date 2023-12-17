f23bcde30c24e5a9ff7406700930b9ee6a409422
daws76 <schandradba9@gmail.com>
2023-12-17 06:34:39 +0530

    new script4

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello: $PERSON2"
+echo "$PERSON2: Hi: $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: How are you $PERSON1"

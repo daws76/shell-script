5a5ec72d562357139c544f4f8ee06d11f3f3475b
daws76 <schandradba9@gmail.com>
2023-12-17 19:14:15 +0530

    new function script

---
+++ 10-function.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+     if [ $1 -ne 0 ]
+     then
+       echo "Error $2 ... Failed"
+    else 
+       echo "$2 ... Success"
+    fi   
+}
+
+if [ $ID -ne 0 ]
+then 
+  echo "Error please run with root user"
+else  
+  echo "You are root user"
+fi 
+
+yum install mysql -y 
+
+VALIDATE $? "Installing MYSQL"
+
+yum install git -y
+
+VALIDATE $? "Installing GIT"

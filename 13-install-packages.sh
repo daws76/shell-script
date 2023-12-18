70acdf012b1ca2f2ef6acbbcd06fcd20fe34a7fb
daws76 <schandradba9@gmail.com>
2023-12-18 06:44:01 +0530

    updated package script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -6,6 +6,20 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE 
+
+VALIDATE()
+{
+  if [ $1 -ne 0 ]
+  then 
+    echo -e "$2 .. $R Failed $N"
+  else 
+    echo -e "$2 .. $G Success $N"    
+} 
+
 if [ $ID -ne 0 ]
 then 
   echo -e "$R Error please run with root user $N"
@@ -14,7 +28,20 @@ else
   echo "you are root user"
 fi
 
-echo "All alrguments passed: $@"
+#echo "All alrguments passed: $@"
+# git mysql postfix 
+
+for package in $@
+do 
+  yum list installed $package &>> $LOGFILE #pre checking 
+  if [ $? -ne 0 ]
+  then 
+    yum install $package -y &>> $LOGFILE
+    VALIDATE $? "Installation of $package" # validate 
+  else 
+    echo -e "$package is already installed ... $Y SKIPPING $N"  &>> $LOGFILE  
+  fi 
+done  
 
 
     

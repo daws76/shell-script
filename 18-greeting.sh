2b8dafb0214cce5a313b7d7e9c5ae470e8b18815
daws76 <schandradba9@gmail.com>
2023-12-21 20:35:39 +0530

    updates

---
+++ 18-greeting.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+NAME=""
+WISHES=""
+
+USAGE(){
+    echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
+    echo "Options::"
+    echo " -n, Specify the name (mandatory)"
+    echo " -w, Specify the wishes. ex, Good Morning"
+    echo " -h, Display Help and exit"  
+}
+
+while getopts ":n:w:h" opt; do
+   case $opt in
+       n) NAME="$OPTARG";;
+       w) WISHES="$OPTARG";;
+       \?) echo "invalid options: - "$OPTARG"" >&2; USAGE; exit;;
+       :)  USAGE; exit;; 
+       h) USAGE; exit;; 
+    esac 
+done 
+
+if [ -z "$NAME" ];
+then
+    echo "Error: -n is mandatory."
+    USAGE 
+     exit 1
+ fi  
+
+ echo "Hello $NAME, $WISHES, I have been learning shell script"
+ 
+  
